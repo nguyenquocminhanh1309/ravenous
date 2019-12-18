7a505ace91c151a031b6c5e895c5346908431f3e
nguyenquocminhanh1309 <minhanh.nguyenquoc13@gmail.com>
2019-12-18 08:36:42 -0500

    interact with Yelp API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,55 @@
 import React from 'react';
+import Yelp from '../../util/Yelp';
 import './App.css';
 
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
 
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
+// const business = {
+//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// };
+
+// const businesses = [
+//   business,
+//   business,
+//   business,
+//   business,
+//   business,
+//   business
+// ];
 
 class App extends React.Component {
-  searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: [],
+    }
+    this.searchYelp = this.searchYelp.bind(this);
   }
-
+    
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(business => {
+      this.setState({
+        businesses: business,
+      })
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp}/>
-        <BusinessList business={businesses}/>     
+        <BusinessList business={this.state.businesses}/>     
     </div>
     );
   };
